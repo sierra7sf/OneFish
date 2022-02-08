ff2ccec5b2bdad7602df71dbe2bf022a9fa9bd9f
Jane Urciuoli <jurciuol@bowdoin.edu>
2022-02-08 11:51:32 +0100

    Ring Prefabs Finished

--- Assets/Scripts/ThirdRing.cs
+++ Assets/Scripts/ThirdRing.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class ThirdRing : MonoBehaviour
 {
+
+    public float rotationSpeed;
+    private bool button;
+    private float counter;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        counter = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            counter++;
+            if (counter == 3)
+            {
+                button = true;
+            }
+
+        }
+
+        if (!button)
+        {
+            transform.Rotate(0, 0, 6.0f * rotationSpeed * Time.deltaTime);
+        }
     }
 }
+
